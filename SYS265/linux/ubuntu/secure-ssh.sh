bc7f3fd05d0b41f7b6cde0fa4f475fd1a5ccd5a1
lilynelsen25 <lily.nelsen@mymail.champlain.edu>
2025-02-18 00:04:50 +0000

    Part 2, lab 04, ssh hardening

---
+++ SYS265/linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,6 @@
+# secure-ssh.sh
+# author lilynelsen25
+# creates new ssh user using $1 parameter
+# adds a public key from the local repo or curled from the remote rep.
+# removes root ability to ssh
+echo "ALL YOUR CODE GOES HERE"
